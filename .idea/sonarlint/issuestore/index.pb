5a04d737ff0a8fbc985d2cf51e3da195ffe409e4
sidney <sidneymaleoregis@gmail.com>
2022-09-09 22:31:58 +0200

    We upgrade our search UI

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+p
+@app/src/main/java/com/android/diaspopay/ui/components/Surface.kt,8/6/8665cc8dee602b8f29a512e5b2f45ffc839d6bfb
+o
+?app/src/main/java/com/android/diaspopay/ui/views/HomeAppView.kt,a/0/a0bdac681677f9d0a82c5d0cd92b6ac21dcb14cc
+i
+9app/src/main/java/com/android/diaspopay/ui/theme/Theme.kt,7/f/7fc432a5a17d1af7248da08ff17e485614d103bb
+m
+=app/src/main/java/com/android/diaspopay/ui/views/LoginView.kt,7/1/714b34269f632281c51b7283c567e07feb3201c5
--- app/src/main/java/com/android/diaspopay/ui/views/contact/ContactsSearchViews.kt
+++ app/src/main/java/com/android/diaspopay/ui/views/contact/ContactsSearchViews.kt
@@ -3,47 +3,68 @@ package com.android.diaspopay.ui.views.bottomnavigationviews
 import android.provider.ContactsContract
 import android.util.Log
 import android.widget.Toast
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.ContactPage
 import androidx.compose.material.icons.outlined.History
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.android.diaspopay.R
 import com.android.diaspopay.ui.views.contact.ContactItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ContactsSearchView() {
-    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
     val listState = rememberLazyListState()
+    var searchContact by rememberSaveable { mutableStateOf("") }
 
     read()
     Scaffold(topBar = {
-        SmallTopAppBar(
-            navigationIcon = {
-                Row(modifier = Modifier.padding(10.dp)) {
-                    Icon(
-                        Icons.Outlined.ContactPage,
-                        contentDescription = null
-                    )
-                    Text(
-                        text = "Contacts",
-                        modifier = Modifier.padding(start = 10.dp)
-                    )
-                }
-            },
-            actions = {},
-            scrollBehavior = scrollBehavior,
-            title = {}
-        )
+        Column (
+            modifier = Modifier.background(MaterialTheme.colorScheme.surface)) {
+            OutlinedTextField(
+                value = searchContact,
+                singleLine = true,
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
+                ),
+                onValueChange = { searchContact = it },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                placeholder = { Text(text= stringResource(id = R.string.search), fontSize = 12.sp) },
+                leadingIcon = {
+                    IconButton(onClick = { }) {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, bottom = 10.dp, start = 30.dp, end = 30.dp)
+                    .height(45.dp),
+                shape = RoundedCornerShape(22.dp)
+            )
+        }
     }){innerPadding ->
         LazyColumn(contentPadding = innerPadding, state = listState) {
             items(count = 2000) {
